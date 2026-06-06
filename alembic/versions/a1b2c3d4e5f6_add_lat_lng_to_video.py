be84b6dd45064d268f86624a2f3f066d814f0cb8
우원 <thewoowon@gmail.com>
2026-06-06 16:57:10 +0900

    fix: alembic head migration solved

--- alembic/versions/a1b2c3d4e5f6_add_lat_lng_to_video.py
+++ alembic/versions/a1b2c3d4e5f6_add_lat_lng_to_video.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = 'e96f05a8cae1'
+down_revision: Union[str, None] = 'd9ceb9d1df2a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
